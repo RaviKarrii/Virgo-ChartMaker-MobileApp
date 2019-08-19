cbd911978b2dd5ed5cf9d9799144cd1e6899e45f
RaviKarrii <me@ravikarri.in>
2019-08-20 00:17:15 +0530

    Added Timepicker

--- Virgo/App.js
+++ Virgo/App.js
@@ -1,7 +1,8 @@
 import React,{ Component}  from 'react';
-import { StyleSheet, Text ,TextInput , View } from 'react-native';
+import { StyleSheet, Text ,TextInput , View, TouchableOpacity } from 'react-native';
 import  Chart  from './chart/chart';
 import DatePicker from 'react-native-datepicker';
+import TimePicker from "react-native-24h-timepicker";
 
 export default class Test extends Component{
 
@@ -13,8 +14,18 @@ export default class Test extends Component{
       date_in: '2016-05-01',
       date_out: '2016-05-01',
       Name : 'Name',
+      time: "00:00",
     
      };
+  }
+
+  onCancel() {
+    this.TimePicker.close();
+  }
+ 
+  onConfirm(hour, minute) {
+    this.setState({ time: `${hour}:${minute}` });
+    this.TimePicker.close();
   }
 
   onProjectSelect = (e) => {
@@ -26,16 +37,21 @@ export default class Test extends Component{
   render(){
     return (
       <View style={styles.container}>
-      <Text>Name </Text>
+      <Text >Name</Text>
       <TextInput name={this.state.Name}
-      onChangeText={(Name) => {this.setState({Name: Name});}}/>
+      onChangeText={(Name) => {this.setState({Name: Name});}}
+      style={{backgroundColor:'blue',color:'white'}}
+      >           </TextInput>
+      <Text>DoB </Text>
       <DatePicker
+      confirmBtnText="Confirm"
+      cancelBtnText="Cancel"
       style ={{padding:10}}
       date={this.state.date_in}
       mode="date"
       format="YYYY-MM-DD"
-      minDate="2016-05-01"
-      maxDate="2020-06-01"
+      minDate="1930-01-01"
+      maxDate="2040-01-01"
       showIcon={false}
       customStyles={{
        dateInput: {
@@ -44,6 +60,19 @@ export default class Test extends Component{
       },
      }}
     onDateChange={(date_in) => {this.setState({date_in: date_in});}}/>
+    <TouchableOpacity
+          onPress={() => this.TimePicker.open()}
+          style={styles.button}>
+          <Text style={styles.buttonText}>{this.state.time}</Text>
+        </TouchableOpacity>
+        
+        <TimePicker
+          ref={ref => {
+            this.TimePicker = ref;
+          }}
+          onCancel={() => this.onCancel()}
+          onConfirm={(hour, minute) => this.onConfirm(hour, minute)}
+        />
     </View>
     
     )
